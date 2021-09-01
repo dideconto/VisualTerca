2ceeff7604770182f16865a6ca503edcf764e754
Diogo Deconto <dideconto@gmail.com>
2021-08-31 22:48:55 -0300

    CRUD de Produtos com códigos HTTP na resposta

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -20,17 +20,58 @@ namespace API.Controllers
         //POST: api/produto/create
         [HttpPost]
         [Route("create")]
-        public Produto Create(Produto produto)
+        public IActionResult Create([FromBody] Produto produto)
         {
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-            return produto;
+            return Created("", produto);
         }
 
         //GET: api/produto/list
         [HttpGet]
         [Route("list")]
-        public List<Produto> List() => _context.Produtos.ToList();
+        public IActionResult List() => Ok(_context.Produtos.ToList());
 
+        //GET: api/produto/getbyid/1
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Produto produto = _context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        //DELETE: /api/produto/delete/bolacha
+        [HttpDelete]
+        [Route("delete/{name}")]
+        public IActionResult Delete([FromRoute] string name)
+        {
+            //Expressão lambda
+            //Buscar um objeto na tabela de produtos com base no nome
+            Produto produto = _context.Produtos.FirstOrDefault(produto => produto.Nome == name);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(_context.Produtos.ToList());
+        }
+
+
+        //PUT: api/produto/update
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update([FromBody] Produto produto)
+        {
+            _context.Produtos.Update(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
     }
 }
