d37f707c38476b5809fdd6676d973510fef712fa
Diogo Deconto <dideconto@gmail.com>
2021-09-21 22:26:39 -0300

    Configuração de CORS para qualquer método e cabeçalho

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,7 +33,9 @@ namespace API
                 options =>
                 {
                     options.AddPolicy("CorsPolicy", builder => builder
-                        .AllowAnyOrigin());
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
                 }
             );
 
