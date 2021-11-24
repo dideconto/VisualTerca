e8985db620a918e88b4434707472868d97971f41
Diogo Deconto <dideconto@gmail.com>
2021-11-23 22:12:33 -0300

    Implementação da autenticação e autorização por JWT na API - Parte II

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Linq;
 using API.Data;
 using API.Models;
+using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +22,11 @@ namespace API.Controllers
         [Route("create")]
         public IActionResult Create([FromBody] Usuario usuario)
         {
+            Usuario usuarioExistente = _context.Usuarios.FirstOrDefault(u => u.Email == usuario.Email);
+            if (usuarioExistente != null)
+            {
+                return BadRequest(new { message = "Este e-mail já está em uso" });
+            }
             _context.Usuarios.Add(usuario);
             _context.SaveChanges();
             usuario.Senha = "";
@@ -30,7 +38,44 @@ namespace API.Controllers
         [Route("login")]
         public IActionResult Login([FromBody] Usuario usuario)
         {
+            usuario = _context.Usuarios.FirstOrDefault
+                (u => u.Email == usuario.Email && u.Senha == usuario.Senha);
+            if (usuario == null)
+            {
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+            }
+
+            usuario.Token = TokenService.CriarToken(usuario);
+            usuario.Senha = "";
             return Ok(usuario);
+        }
+
+        // GET: api/usuario/sem
+        [HttpGet]
+        [Route("sem")]
+        [AllowAnonymous]
+        public IActionResult Sem()
+        {
+            return Ok(new { message = "Sem autenticação" });
+        }
+
+        // GET: api/usuario/com
+        [HttpGet]
+        [Route("com")]
+        [Authorize]
+        public IActionResult Com()
+        {
+            Usuario usuario = _context.Usuarios.FirstOrDefault(u => u.Email == User.Identity.Name);
+            return Ok(new { identity = User.Identity.Name, user = usuario });
+        }
+
+        // GET: api/usuario/permissao
+        [HttpGet]
+        [Route("permissao")]
+        [Authorize(Roles = "adm")]
+        public IActionResult Permissao()
+        {
+            return Ok(new { message = "Administrador" });
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -44,6 +47,25 @@ namespace API
             );
 
             services.AddControllers();
+
+            byte[] key = Encoding.ASCII.GetBytes(Settings.secret);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -65,6 +87,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
